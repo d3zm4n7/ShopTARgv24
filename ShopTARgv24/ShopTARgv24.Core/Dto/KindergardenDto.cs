6d05793d6796f0071af0b668558f5553bb173f70
Kirill.Melnikov <dezxplay@gmail.com>
2025-09-19 08:55:15 +0300

    Kindergarden Index + CreateUpdate implemented

---
+++ ShopTARgv24/ShopTARgv24.Core/Dto/KindergardenDto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ShopTARgv24.Core.Dto
+{
+    public class KindergardenDto
+    {
+        public Guid? Id { get; set; }
+        public string? GroupName { get; set; }
+        public int? ChildrenCount { get; set; }
+        public string? KindergardenName { get; set; }
+        public string? TeacherName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
--- ShopTARgv24/ShopTARgv24/Controllers/KindergardenController.cs
+++ ShopTARgv24/ShopTARgv24/Controllers/KindergardenController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShopTARgv24.Data;
+using ShopTARgv24.Models.Kindergarden;
 
 namespace ShopTARgv24.Controllers
 {
     public class KindergardenController : Controller
     {
+        private readonly ShopTARgv24Context _context;
+        public KindergardenController
+            (
+            ShopTARgv24Context context
+            )
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Kindergardens
+                .Select(x => new KindergardenIndexViewModel
+                {
+                    Id = x.Id,
+                    GroupName = x.GroupName,
+                    ChildrenCount = x.ChildrenCount,
+                    KindergardenName = x.KindergardenName,
+                    TeacherName = x.TeacherName
+                });
+            return View(result);
         }
     }
 }
