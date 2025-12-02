429f1bb667189e91f79c90ba7d3995e5ba1e2d58
DEZTERMINAL\dezxp <dezxplay@gmail.com>
2025-12-02 12:29:41 +0200

    chat 0

---
+++ ShopTARgv24/ShopTARgv24/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace ShopTARgv24.Hubs
+{
+    // Наследуемся от базового класса Hub
+    public class ChatHub : Hub
+    {
+        // Метод, который будут вызывать клиенты
+        public async Task SendMessage(string user, string message)
+        {
+            // Отправляем сообщение всем подключенным клиентам
+            // "ReceiveMessage" — это имя метода, который мы будем слушать в JavaScript
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
